e621377f3f8ba72db5af165fdcbbea241f1b3ed5
Delian Delchev <delian.delchev@gmail.com>
2019-03-12 15:15:24 +0100

    Send only the coordinates that makes sense and change
    Signed-off-by: Delian Delchev <delian.delchev@gmail.com>

--- node/src/retrieveBackend.js
+++ node/src/retrieveBackend.js
@@ -12,8 +12,8 @@ let url = "http://127.0.0.1:8090/world";
 let planesUrl = "https://opensky-network.org/api/states/all";
 
 let userState = {};
-
 let oldMarkers = {};
+let oldPlanes = {};
 
 function broadcast() {
     request(url, (err, resp, body) => {
@@ -74,14 +74,25 @@ function broadcast() {
         console.log('Some planes data');
 
         let d = data.states.sort((a,b) => a[0]<b[0] ? -1:1).slice(0,50).map(n => {
-            return {
+            let obj = {
                 id: n[0],
                 n: n,
-                latlng: [n[6]||0, n[5]||0],
+                latlng: [n[6], n[5]],
                 deg: n[10] || 0,
-                speed: n[9]
+                speed: n[9],
+                change: true
+            };
+            if (oldPlanes[obj.id] && (oldPlanes[obj.id].latlng[0] != obj.latlng[0]|| oldPlanes[obj.id].latlng[1] != obj.latlng[1])) {
+                obj.change = true;
+            } else {
+                obj.change = false;
             }
-        });
+            if (n[6] == null || n[5] == null || n[6] === 0 || n[5] === 0) {
+                obj.change = false;
+            }
+            oldPlanes[obj.id] = obj;
+            return obj;
+        }).filter(n => n.change);
 
         io.broadcast('planes', d);
     })
