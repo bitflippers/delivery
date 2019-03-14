aa600fec981872f2ec61cd84481a57cdcbd844f6
Delian Delchev <delian.delchev@gmail.com>
2019-03-14 12:55:22 +0100

    Small refactoring of the log - filtering
    Signed-off-by: Delian Delchev <delian.delchev@gmail.com>

--- webapp/src/app/footer/footer.component.ts
+++ webapp/src/app/footer/footer.component.ts
@@ -1,7 +1,14 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {MessagingService} from "../messaging/messaging.service";
 import {SpinnerServiceService} from "../spinner-service/spinner-service.service";
 import {iconList} from "../ressources/iconConvertor";
+
+
+const colorMap = {
+  "SYSTEM": "gray",
+  "SADREMA": "red",
+  "EVENTS": "blue"
+};
 
 @Component({
   selector: 'app-navigation',
@@ -11,18 +18,70 @@ import {iconList} from "../ressources/iconConvertor";
 export class FooterComponent implements OnInit {
 
   saDReMalog = [];
+  saDReMaFullLog = [];
   lastMsg = '';
+  paused = false;
+  pausedBuff = [];
+
+  showSystem = true;
+  showEvents = true;
+  showSadrema = true;
+
+  @ViewChild('scrollEl') scrEl;
 
   constructor(public msg: MessagingService, public spinner: SpinnerServiceService) {
+  }
+
+  trackByData(index: number, data) {
+    return data;
+  }
+
+  togglePause() {
+    this.paused = !this.paused;
+  }
+
+  toggleSystem() {
+    this.showSystem = !this.showSystem;
+    this.toggleFilter();
+  }
+
+  toggleEvents() {
+    this.showEvents = !this.showEvents;
+    this.toggleFilter();
+  }
+
+  toggleSADREMA() {
+    this.showSadrema = !this.showSadrema;
+    this.toggleFilter();
+  }
+
+  toggleFilter() {
+    this.saDReMalog = this.saDReMaFullLog
+      .filter(n => this.showSystem || (!n.text.match(/\[SYSTEM\]/)))
+      .filter(n => this.showEvents || (!n.text.match(/\[EVENTS\]/)))
+      .filter(n => this.showSadrema || (!n.text.match(/\[SADREMA\]/)));
+    if (this.saDReMalog.length > 0) {
+      this.lastMsg = this.saDReMalog[this.saDReMalog.length - 1].text;
+    }
   }
 
   ngOnInit() {
     const r = /\[/;
     this.msg.messages.subscribe(data => {
       //console.log('Messages', data);
-      this.saDReMalog = this.saDReMalog.concat(data.filter(n => r.test(n)).map(n => ({ text: n, color: 'red' }))).splice(-15);
-      this.lastMsg = this.saDReMalog[this.saDReMalog.length - 1].text;
+      for (const line of data.filter(n => r.test(n)).map(n => ({ text: n, color: colorMap[n.match(/\[(\w+)\]/)[1]] }))) {
+        if (this.saDReMaFullLog.concat(this.pausedBuff).filter(n => n.text === line.text).length > 0) {
+          continue;
+        }
+        this.pausedBuff.push(line);
+      }
+      if (!this.paused) {
+        this.saDReMaFullLog = this.saDReMalog.concat(this.pausedBuff).splice(-100);
+        this.scrEl.nativeElement.scrollTop = this.scrEl.nativeElement.scrollHeight;
+      }
+      this.toggleFilter(); // Check on it
     });
   }
 
 }
+
