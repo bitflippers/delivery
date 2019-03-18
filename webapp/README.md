506591febb267081bbf182ee87717c61e69d1868
Delian Delchev <delian.delchev@gmail.com>
2019-03-18 15:22:48 +0100

    Migration to Angular 7 and Angular Material 7
    Signed-off-by: Delian Delchev <delian.delchev@gmail.com>

--- webapp/README.md
+++ webapp/README.md
@@ -1,6 +1,6 @@
 # Webapp
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.2.9.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.3.6.
 
 ## Development server
 
--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -1,42 +1,44 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import {FormsModule} from '@angular/forms';
 
-import {AppComponent} from './app.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MaterialModuleModule} from './material-module/material-module.module';
 import {LayoutModule} from '@angular/cdk/layout';
-import {WorldMapComponent} from './world-map/world-map.component';
-import {UserLoginComponent, LoginDialogComponent} from './user-login/user-login.component';
-import {UserUiComponent} from './user-ui/user-ui.component';
-import {SpinnerComponent} from './spinner/spinner.component';
-import {UserListComponent} from './user-list/user-list.component';
-import {MarkerButtonComponent, MarkerDialogComponent} from './marker-button/marker-button.component';
-import {FooterComponent} from './footer/footer.component';
-import {HomeButtonComponent} from './home-button/home-button.component';
-import {DisasterComponent} from './disaster/disaster.component';
+import { WorldMapComponent } from './world-map/world-map.component';
+import { UserUiComponent } from './user-ui/user-ui.component';
+import { UserLoginComponent, LoginDialogComponent } from './user-login/user-login.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { SpinnerComponent } from './spinner/spinner.component';
+import { MarkerButtonComponent, MarkerDialogComponent } from './marker-button/marker-button.component';
+import { HomeButtonComponent } from './home-button/home-button.component';
+import { FooterComponent } from './footer/footer.component';
+import { DisasterComponent } from './disaster/disaster.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WorldMapComponent,
+    UserUiComponent,
     UserLoginComponent,
     LoginDialogComponent,
-    UserUiComponent,
-    SpinnerComponent,
     UserListComponent,
+    SpinnerComponent,
     MarkerButtonComponent,
     MarkerDialogComponent,
-    FooterComponent,
     HomeButtonComponent,
-    DisasterComponent,
+    FooterComponent,
+    DisasterComponent
   ],
   entryComponents: [
     MarkerDialogComponent,
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModuleModule,
     LayoutModule,
@@ -45,5 +47,4 @@ import {DisasterComponent} from './disaster/disaster.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule { }
